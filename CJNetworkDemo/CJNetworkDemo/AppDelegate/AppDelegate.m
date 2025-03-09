cae5b7b4d917acd392a1ae0e04483f5a1b252b5a
dvlproad <dvlproad@163.com>
2025-03-09 16:36:53 +0800

    【Feature】修改icon和启动图

--- CJNetworkDemo/CJNetworkDemo/AppDelegate/AppDelegate.m
+++ CJNetworkDemo/CJNetworkDemo/AppDelegate/AppDelegate.m
@@ -40,9 +40,9 @@
     [self.window settingRoot];
     
     
-    [CJLogSuspendWindow showWithFrame:CGRectMake(10, 200, 100, 100) configBlock:^(CJLogSuspendWindow *bSuspendWindow) {
-        
-    }];
+//    [CJLogSuspendWindow showWithFrame:CGRectMake(10, 200, 100, 100) configBlock:^(CJLogSuspendWindow *bSuspendWindow) {
+//        
+//    }];
     
     return YES;
 }
